f0f493143f01e5f77da1b7e398327baef7535a19
LinZiYu1996 <2669093302@qq.com>
2020-01-23 22:48:07 +0800

    J-1-23

---
+++ MyNotes/thread/c3/c_3_1/c_3_1_7/A5/A5_3/Test.java
@@ -0,0 +1,34 @@
+package thread.c3.c_3_1.c_3_1_7.A5.A5_3;
+
+import java.util.HashMap;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: LinZiYu
+ * \* Date: 2020/1/23
+ * \* Time: 21:09
+ * \* Description:
+ * \
+ */
+public class Test {
+
+
+//    java中有三种移位运算符
+//
+//<<      :     左移运算符，num << 1,相当于num乘以2
+//
+//>>      :     右移运算符，num >> 1,相当于num除以2
+//
+//>>>    :     无符号右移，忽略符号位，空位都以0补齐
+
+    public static void main(String[] args) {
+
+        System.out.println(10>>>1);
+
+        System.out.println(10<<1);
+
+
+        HashMap hashMap = new HashMap();
+
+    }
+}
